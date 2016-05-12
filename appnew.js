88a85abe9d3481437c6bc3780ce044969da8e796
lummish <lummish@gmail.com>
2016-05-12 16:03:01 -0700

    changed reader syntax

--- appnew.js
+++ appnew.js
@@ -50,10 +50,11 @@ function convertImgToDataURLviaFileReader(url, callback) {
   var xhr = new XMLHttpRequest();
   xhr.responseType = 'blob';
   xhr.onload = function() {
-    FileReader.onloadend = function() {
+    var reader = new FileReader();
+    reader.onloadend = function() {
       callback(reader.result);
     }
-    FileReader.readAsDataURL(xhr.response);
+    reader.readAsDataUR(xhr.response);
   }
   xhr.open('GET', url);
   xhr.send();
