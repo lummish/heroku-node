c2ce450aa7975ee9d7f945a8f4e03bba5c6fd5a3
lummish <lummish@gmail.com>
2016-05-12 15:42:58 -0700

    testing to see if message being passed to webhook

--- appnew.js
+++ appnew.js
@@ -108,6 +108,7 @@ app.post('/webhook/', function (req, res) {
   for (i = 0; i < messaging_events.length; i++) {
   	event = req.body.entry[0].messaging[i];
     sender = event.sender.id;
+    console.log(event);
     if (event.message && event.message.text) {
       text = event.message.text;
       // Handle a text message from this sender
