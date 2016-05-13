ee9c3dc825d2940cc03cc8577838e1745b3ff058
lummish <lummish@gmail.com>
2016-05-12 18:18:02 -0700

    trying with simple url

--- appnew.js
+++ appnew.js
@@ -164,7 +164,7 @@ app.post('/webhook/', function (req, res) {
         b64img = img;
       });
 
-      processImageFromB64(b64img);
+      processImageFromB64(imgUrl);
       //download(imgUrl, filename, function() { //need file extension
       //  console.log("Downloaded.");
         //var bucket = gcs.bucket('receipt-read-bucket');
